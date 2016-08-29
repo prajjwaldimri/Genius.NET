93aa6729a5da7fe20bc2f311538d8a1a11661d89
Prajjwal <prajjwaldimri@hotmail.com>
2016-08-29 08:09:53 +0530

    Added OAuth Implementation and test projects

---
+++ Genius/Genius.NET.TestUWPApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Genius.NET.TestUWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Authenticator.ClientId = "CLIENT_ID";
+            Authenticator.RedirectUri = "https://genius.com";
+            Authenticator.Scope = "me";
+            Authenticator.State = "default_state";
+            Authenticator.ClientSecret = "ENTER_CLIENT_SECRET";
+            var url = Authenticator.GetAuthenticationUrl();
+            WebView.FrameContentLoading += web_FrameContentLoading;
+            WebView.NavigationCompleted += web_NavigationCompleted;
+            WebView.Navigate(url);
+        }
+
+        private static void web_FrameContentLoading(WebView sender, WebViewContentLoadingEventArgs args)
+        {
+            Debug.WriteLine("Yo");
+        }
+
+        private static async void web_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            var uri = args.Uri.ToString();
+            if (uri.Contains("code="))
+            {
+                await Authenticator.GetAccessToken(args.Uri);
+            }
+        }
+    }
+}
---
+++ Genius/Genius.NET.Tests/AuthenticatorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Genius.NET.Tests
+{
+    [TestClass]
+    public class AuthenticatorTests
+    {
+        [TestMethod]
+        public void GetAuthenticationUrlCorrect()
+        {
+            Authenticator.ClientId = "clientId";
+            Authenticator.RedirectUri = "redirectUri";
+            Authenticator.Scope = "scope";
+            Authenticator.State = "state";
+            Assert.AreEqual(new Uri("https://api.genius.com/oauth/authorize?client_id=clientId&redirect_uri=redirectUri" +
+                            "&scope=scope&state=state&response_type=code"), Authenticator.GetAuthenticationUrl());
+        }
+
+        [TestMethod]
+        public void GetAuthenticationUrlInCorrect()
+        {
+            Authenticator.ClientId = null;
+            Authenticator.RedirectUri = null;
+            Authenticator.Scope = null;
+            Authenticator.State = null;
+            Assert.ThrowsException<ArgumentNullException>(Authenticator.GetAuthenticationUrl);
+        }
+    }
+}
