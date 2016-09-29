4164468f43e5be030ee7553399697b67d264ef83
Prajjwal <prajjwaldimri@hotmail.com>
2016-09-29 20:30:16 +0530

    Change assembly version

--- Genius/Properties/AssemblyInfo.cs
+++ Genius/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Genius")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Genius.NET")]
+[assembly: AssemblyDescription(".NET library to access Genius API @ https://genius.com")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Genius")]
+[assembly: AssemblyProduct("Genius.NET")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
