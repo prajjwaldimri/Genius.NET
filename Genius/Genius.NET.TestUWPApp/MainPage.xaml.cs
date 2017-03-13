241b65c2a597bd136e81fb12f5a65e1d46db5637
Prajjwal <prajjwaldimri@hotmail.com>
2017-03-13 13:14:33 +0530

    Removed Unused References

--- Genius/Genius.NET.TestUWPApp/MainPage.xaml.cs
+++ Genius/Genius.NET.TestUWPApp/MainPage.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
