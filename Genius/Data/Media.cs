3d89457e7a91c12da4eb0cc09976c444e620990e
Michael Plainer <plainer@ymail.com>
2017-11-24 15:19:16 +0100

    Fix weird spacing in media class

--- Genius/Data/Media.cs
+++ Genius/Data/Media.cs
@@ -1,11 +1,11 @@
 ﻿namespace Genius.Data
 {
 	public class Media
-    {
-        public bool Cinema { get; set; }
-        public string Provider { get; set; }
-        public string Start { get; set; }
-        public string Type { get; set; }
-        public string Url { get; set; }
-    }
+	{
+		public bool Cinema { get; set; }
+		public string Provider { get; set; }
+		public string Start { get; set; }
+		public string Type { get; set; }
+		public string Url { get; set; }
+	}
 }
