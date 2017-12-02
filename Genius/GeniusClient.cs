c6f5974632a307e6a61aa264966ac82a290d0610
Prajjwal <prajjwaldimri@hotmail.com>
2017-12-02 21:56:19 +0530

    GeniusClient class will be used as a wrapper around whole library

---
+++ Genius/GeniusClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Genius
+{
+    /// <summary>
+    ///     A client for Genius API
+    /// </summary>
+    public class GeniusClient
+    {
+        /// <summary>
+        ///     Creates a new instance of GeniusClient
+        /// </summary>
+        /// <param name="accessToken">Access Token to make authorized requests.</param>
+        public GeniusClient(string accessToken)
+        {
+            AccessToken = accessToken;
+        }
+
+        /// <summary>
+        ///     https://docs.genius.com/#/authentication-h1
+        /// </summary>
+        public string AccessToken { get; set; }
+    }
+}
