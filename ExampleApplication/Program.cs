5744dc276b9701774536d5ca684cbd69bd8821a9
Prajjwal <prajjwaldimri@hotmail.com>
2017-12-02 21:55:58 +0530

    Added basic example application to show example usage of library

---
+++ ExampleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using Genius;
+
+namespace ExampleApplication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var geniusClient = new GeniusClient("API_TOKEN");
+        }
+    }
+}
