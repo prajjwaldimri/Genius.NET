df6926a83a402396d512c2ec2e65e53ab6f0fc43
Prajjwal <prajjwaldimri@hotmail.com>
2017-12-03 20:19:48 +0530

    Added Annotation Payload

---
+++ Genius/Models/AnnotationPayload.cs
@@ -0,0 +1,9 @@
+﻿namespace Genius.Models
+{
+    public class AnnotationPayload
+    {
+        public Annotation Annotation;
+        public Referent Referent;
+        public WebPage WebPage;
+    }
+}
