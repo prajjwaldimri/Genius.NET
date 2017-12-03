5bb89ef4c55a80f5d9d02319b1398ff948c99b35
Prajjwal <prajjwaldimri@hotmail.com>
2017-12-03 12:18:54 +0530

    Added UriHelper to create URI's based on requests easily

---
+++ Genius/Helpers/UriHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Genius.Helpers
+{
+    internal static class UriHelper
+    {
+        public static Uri CreateUri<T>(string textFormat, string id = "")
+        {
+            return string.IsNullOrWhiteSpace(id)
+                ? new Uri($"https://api.genius.com/{typeof(T).Name.ToLower()}s?text_format={textFormat}")
+                : new Uri($"https://api.genius.com/{typeof(T).Name.ToLower()}s/{id}?text_format={textFormat}");
+        }
+    }
+}
