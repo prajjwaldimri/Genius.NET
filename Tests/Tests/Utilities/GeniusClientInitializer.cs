ace050c2f9e60501475d86021fdbd1e7bfbe9604
Omar Pani <o.pani@avanade.com>
2021-02-07 18:30:59 +0100

    Improved unit testing

---
+++ Tests/Tests/Utilities/GeniusClientInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Genius;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Tests.Utilities
+{
+    public static class GeniusClientInitializer
+    {
+        public static GeniusClient GetClient()
+        {
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration("Tests.dll");
+            string apiKey = configuration.AppSettings.Settings["API_KEY"]?.Value;
+            return new GeniusClient(apiKey);
+        }
+    }
+}
